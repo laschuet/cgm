57902c56d3b2f8fa39e9f6b9be34739a5cf8a960
laschuet <laschuet@users.noreply.github.com>
2019-01-09 20:14:01 +0100

    Remove empty line after the license

--- cgm/cgm.h
+++ cgm/cgm.h
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-
 #ifndef CGM_H_INCLUDE
 #define CGM_H_INCLUDE
 
